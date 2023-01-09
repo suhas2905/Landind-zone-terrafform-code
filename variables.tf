6097bb7fb9076dde4c608a4f5037fe1d66bc29cf
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-09 05:37:06 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,3 +28,25 @@ variable "availability_zones" {
     type = list
     default = ["us-east-2a" , "us-east-2b"]   #defining AZs for Subnets
 }
+
+variable "instance_type" {
+  description = "(Required) AWS instance_type to execute deployment to"
+  default     = "m5.large"
+  type        = string  
+}
+
+variable "ami" {
+  description = "(Required) The AMI id for the underlying OS"
+  default     = "ami-092b43193629811af"
+  type        = string
+}
+
+variable "key_name" {
+  description = "(Optional) The key pair name for the instances. If not provided - you can use SSM session manager for console access"
+  default     = "sap-us-east-2"
+  type        = string
+}
+variable "user_data" {
+  description = "(Optional) The user data script for the instance. If none provisioned - default one will be used to install AWS CLI and SSM agent"
+  default     = ""
+}
