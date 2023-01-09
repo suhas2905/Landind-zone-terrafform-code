0a89d35d956e04ca9386389003384b97d6769c1b
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-09 05:38:09 -0500

    Update subnet_association.tf

--- modules/aws-landing-zone/subnet_association.tf
+++ modules/aws-landing-zone/subnet_association.tf
@@ -5,3 +5,10 @@ resource "aws_route_table_association" "private_sub" {
     route_table_id = aws_route_table.private_route_table.id
 
 }
+resource "aws_route_table_association" "public_sub" {
+
+    count          = length(var.subnets_cidr)
+    subnet_id      = element(aws_subnet.public_subnet.*.id , count.index)
+    route_table_id = aws_route_table.public_route_table.id
+
+}
