6eed4447d6658a7f64d3e8d77a5a4dcf94b15c5c
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-09 05:36:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ module "sap_landing_zone" {
   subnets_cidr              = var.subnets_cidr
   public_subnets_cidr       = var.public_subnets_cidr
   availability_zones        = var.availability_zones
+  ami                       = var.ami
+  instance_type             = var.instance_type
+  key_name                  = var.key_name
+  user_data                 = var.user_data
 }
